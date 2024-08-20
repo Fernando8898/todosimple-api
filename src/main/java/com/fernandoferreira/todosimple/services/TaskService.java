60cc3e641a7b8f7e7a04ddb687c5a5065b62e1ce
Fernando8898 <131680054+Fernando8898@users.noreply.github.com>
2024-08-20 20:54:38 -0300

    Criat taskController

--- src/main/java/com/fernandoferreira/todosimple/services/TaskService.java
+++ src/main/java/com/fernandoferreira/todosimple/services/TaskService.java
@@ -1,5 +1,6 @@
 package com.fernandoferreira.todosimple.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.apache.catalina.User;
@@ -25,6 +26,15 @@ public class TaskService {
         return task.orElseThrow(() -> new RuntimeException(
             "Tarefa não encontrada! Id: " + id + ", Tipo " + Task.class.getName()));
     }
+
+    
+    public List<Task> findAllByUserId(Long userId){
+        List<Task> tasks = this.taskRepository.findByUser_Id(userId);
+        return tasks;
+        
+    }
+
+    
     @Transactional
     public Task create (Task obj){
         User user = (User) this.userService.findById(obj.getUser().getId());
